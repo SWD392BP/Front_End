c3abc7d7a7a60d46437199c26bee0cedac108d41
dgphus <dphus1507@gmail.com>
2024-03-10 20:20:22 +0700

    Create room menu on host management

---
+++ kid-web-app/src/types.ts
@@ -0,0 +1,50 @@
+export interface JsonBody {
+    code: string,
+    message: string,
+    data: any,
+    totalItem: number,
+    totalPage: number,
+}
+
+export interface User {
+    UserID: number,
+    FullName: string,
+    Email: string,
+    Password: string,
+    Role: string,
+    Status: string,
+    Token: string,
+}
+
+export interface Party {
+    partyID: number,
+    partyName: string,
+    address: string,
+    type: string,
+    description: string,
+    image: string,
+    hostUserId: number,
+}
+
+
+export interface UserInfoCookie{
+    userID: number;
+    email: string;
+    token: string;
+    role: string;
+}
+
+export interface PartyType{
+    label: string;
+    value: string;
+}
+
+export interface ButtonPagination {
+    page: number;
+    isActive: boolean;
+}
+
+export type Option = {
+    value: string,
+    label: string
+}
